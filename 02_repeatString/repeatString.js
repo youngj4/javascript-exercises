23d4c5f0aa61985b4a64287fb1504c2dc029d06a
youngj4 <john@rapidrecon.com>
2024-09-03 16:01:00 -0700

    solves exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let i = 0;
+    let result = '';
+    if (num < 0) {
+        return 'ERROR'
+    }
+    while (i < num) {
+        result += string;
+        i++;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
