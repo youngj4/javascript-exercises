e5b52a4889f7f9dd046d4ef30715819ee70b9af0
youngj4 <john@rapidrecon.com>
2024-09-04 17:59:40 -0700

    solves test 1 of exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, inputValue) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let value = array.at(i);
+        if (value !== inputValue) {
+            newArray.push(value);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
